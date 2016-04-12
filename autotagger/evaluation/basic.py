744484909b99ca4939032e54b989a39883b98330
queirozfcom <falmeida1988@gmail.com>
2016-04-12 02:16:43 -0300

    new commit (automatic message)

--- autotagger/evaluation/basic.py
+++ autotagger/evaluation/basic.py
@@ -8,7 +8,7 @@ def build_dummy_pred_test_pair():
     builds a dummy pair of binary multilabel predictions and actual labels, whose
     micro-averaged f1 score must equal 0.7
 
-    :return: micro f1 score
+    :return: dummy data pair to use in examples
     """
 
     pred_y = [
@@ -30,6 +30,27 @@ def get_micro_f1(predictions, test_set):
     assert len(predictions) == len(
             test_set), "there must be an equal number of elements in both sets"
 
+    sum_tp = 0
+    sum_fn = 0
+    sum_fp = 0
+
+    for pred, test in zip(predictions, test_set):
+        sum_tp += __get_tp(pred, test)
+        sum_fn += __get_fn(pred, test)
+        sum_fp = __get_fp(pred, test)
+
+    micro_prec = sum_tp / (sum_tp + sum_fp)
+    micro_recall = sum_tp / (sum_tp + sum_fn)
+
+    micro_f1 = 2 * micro_prec * micro_recall / (micro_prec + micro_recall)
+
+    return micro_f1
+
+
+def get_samples_f1(predictions, test_set):
+    assert len(predictions) == len(
+            test_set), "there must be an equal number of elements in both sets"
+
     sum_f1 = 0
     num_elements = len(predictions)
 
@@ -43,6 +64,36 @@ def get_micro_f1(predictions, test_set):
         return 0
     else:
         return sum_f1 / num_elements
+
+
+def __get_tp(pred, test):
+    tp = 0
+
+    for p, t in zip(pred, test):
+        if p == 1 and t == 1:
+            tp += 1
+
+    return tp
+
+
+def __get_fn(pred, test):
+    fn = 0
+
+    for p, t in zip(pred, test):
+        if p == 0 and t == 1:
+            fn += 1
+
+    return fn
+
+
+def __get_fp(pred, test):
+    fp = 0
+
+    for p, t in zip(pred, test):
+        if p == 1 and t == 0:
+            fp += 1
+
+    return fp
 
 
 def __get_precision(predictions, test_set):
