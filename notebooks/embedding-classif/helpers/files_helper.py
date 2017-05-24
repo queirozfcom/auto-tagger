9881bddeaa65231845f13207a70d85ce65e08e39
Felipe Almeida <queirozfcom@gmail.com>
2017-05-24 03:21:24 -0300

    new commit (automatic message)

--- notebooks/embedding-classif/helpers/files_helper.py
+++ notebooks/embedding-classif/helpers/files_helper.py
@@ -60,6 +60,41 @@ def read_glove(d=100):
             
     return embeddings_index
 
+def read_glove_weighted(d,weight_index):
+    supported_dimensions = [50,100,200,300]
+    
+    if d not in supported_dimensions:
+        raise ValueError("argument d must be one of {0}".format(",".join(supported_dimensions)))
+    
+    
+    GLOVE_DIR = "/media/felipe/SAMSUNG/GloVe"
+    embeddings_index = {}
+
+    matches = 0
+    overall = 0
+    
+    with open(os.path.join(GLOVE_DIR,"glove.6B.{0}d.txt".format(d)),'r') as f:
+        for line in f:
+            values = line.split()
+            word = values[0]       
+            coefs = np.asarray(values[1:],dtype='float32')
+
+            maybe_weight = weight_index.get(word)
+            
+            if maybe_weight is None:
+                weight = 1.0
+            else:
+                weight = maybe_weight
+                matches += 1
+                        
+            overall +=1        
+            embeddings_index[word] = coefs * weight
+           
+    print("overall, {0} out of {1} embeddings were weighted".format(matches,overall))
+    
+    return embeddings_index    
+      
+
 def read_stackoverflow_sample_small():
     path_to_file = "/media/felipe/SAMSUNG/StackHeavy/Posts-shuffled/Small-Sample-Posts-Shuffled.csv"
 
