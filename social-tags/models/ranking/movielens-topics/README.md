f14fd74c678484742e663fc286eb8edf30e03b7a
Felipe Almeida <queirozfcom@gmail.com>
2018-04-01 00:01:26 -0300

    minor commit (automatic message)

---
+++ social-tags/models/ranking/movielens-topics/README.md
@@ -0,0 +1,1 @@
+there's just one file for both coubey models because the only thing I need to save is the lda model, and it's the same for both
--- social-tags/src/helpers/topics.py
+++ social-tags/src/helpers/topics.py
@@ -1,18 +1,44 @@
-def get_top_words_for_topic(model, topic_index, feature_names, n_top_words):
+import numpy as np
+
+
+def get_word_weight_dict(fitted_model, topic_index, vocabulary):
+    word_weights = fitted_model.components_[topic_index]
+
+    tpls = [(vocabulary[i], w) for (i, w) in enumerate(word_weights)]
+
+    return dict(tpls)
+
+
+def _get_top_words_for_topic(fitted_model, topic_index, vocabulary, n_top_words=None):
     """
     For a given LDA model (trained) and a topic index, return the N most important
     words for that topic.
 
     :param model: topic model such as sklearn.decomposition.LatentDirichletAllocation
     :param topic_index: 0-indexed integer
-    :param feature_names: list of words in the vocabulary (e.g. call get_feature_names() on a trained
+    :param vocabulary: list of words in the vocabulary (e.g. call get_feature_names() on a trained
         sklearn.feature_extraction.text.CountVectorizer)
-    :param n_top_words: number of words to return
+    :param n_top_words: number of words to return. by default, return all
     :return: list of the top words
     """
 
-    topic = model.components_[topic_index]
+    topic = fitted_model.components_[topic_index]
 
-    words = [feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
+    if n_top_words is not None:
+        words = [vocabulary[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
+    else:
+        words = [vocabulary[i] for i in topic.argsort()]
 
     return words
+
+
+def _get_word_topic_distribution(lda_model):
+    """
+    the values in a model component aren't actual conditional word probablities, but rather
+    frequency counts. To make it so that each component actually correspond to the distribution
+    of words in that topic, we must do the follwoing.
+
+    :param lda_model: topic model such as sklearn.decomposition.LatentDirichletAllocation
+    :return:
+    """
+    return lda_model.components_ / lda_model.components_.sum(axis=1)[:, np.newaxis]
