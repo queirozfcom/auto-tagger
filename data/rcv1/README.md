d9c0339648562ceca2d7cd10a02aaf56d353ae7b
Felipe Almeida <queirozfcom@gmail.com>
2018-08-14 23:59:18 -0300

    minor commit (automatic message)

--- data/rcv1/README.md
+++
@@ -1,3 +0,0 @@
-## This is NOT rcv1-v2
-
-It is RCV1-v1, without the enhancements suggested by LEWIS 2004
--- python/autotagger/helpers/datasets.py
+++
@@ -1,8 +0,0 @@
-
-def generate_predictions_maximum_f1():
-    """
-    Generate a dataset partition (Y_pred,Y_test) with perfect predictions
-
-    :return:
-    """
-
